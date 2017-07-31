3da53bf4a37ad125e1bd2a9d81a84c570c2f1514
heoeunsun <gdt2667@daum.net>
2017-07-31 15:42:28 +0900

    Create Goods.java

---
+++ src/model/Goods.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Goods {
+	private String name; // 상품명
+	private int cnt; // 개수
+	private int price; // 가격
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Goods [name=" + name + ", cnt=" + cnt + ", price=" + price + "]";
+	}
+	
+	
+}
